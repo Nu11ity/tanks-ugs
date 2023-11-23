890999cbb01540735140185ac3db58bcc12370c2
Phillip Gonzales <nullitycontact@gmail.com>
2023-11-22 18:31:01 -0800

    Setup leaderboard sorting, and limited it up to the top 8. Also, added a seperate color for the local client, so they can easily distinguish themselves on the leaderboard.

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -10,6 +10,7 @@ public class Leaderboard : NetworkBehaviour
 {
     [SerializeField] Transform leaderboardEntityHolder;
     [SerializeField] LeaderboardEntityDisplay leaderboardEntityPrefab;
+    [SerializeField] int entitiesToDisplay = 8;
 
     private NetworkList<LeaderboardEntityState> leaderboardEntities;
     private List<LeaderboardEntityDisplay> entityDisplays = new List<LeaderboardEntityDisplay>();
@@ -104,6 +105,28 @@ public class Leaderboard : NetworkBehaviour
                     displayToUpdate.UpdateCoins(changeEvent.Value.Coins);
                 }
                 break;
+        }
+
+        entityDisplays.Sort((x, y) => y.Coins.CompareTo(x.Coins));
+
+        for (int i = 0; i < entityDisplays.Count; i++)
+        {
+            entityDisplays[i].transform.SetSiblingIndex(i);
+            entityDisplays[i].UpdateText();
+            bool shouldShow = i <= entitiesToDisplay - 1;
+            entityDisplays[i].gameObject.SetActive(shouldShow);
+        }
+
+        LeaderboardEntityDisplay myDisplay =
+            entityDisplays.FirstOrDefault(x => x.ClientId == NetworkManager.Singleton.LocalClientId);
+
+        if(myDisplay != null)
+        {
+            if(myDisplay.transform.GetSiblingIndex() >= entitiesToDisplay)
+            {
+                leaderboardEntityHolder.GetChild(entitiesToDisplay - 1).gameObject.SetActive(false);
+                myDisplay.gameObject.SetActive(true);
+            }
         }
     }
 
