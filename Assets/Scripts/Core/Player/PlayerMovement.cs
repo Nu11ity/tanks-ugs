03d1c44ae69b7c4ddb7956b4d2ae4911b57e2059
Phillip Gonzales <nullitycontact@gmail.com>
2023-12-07 14:25:51 -0800

    Created trailing effects for tracks, both a treads and smoke effect that fade out over time. Added a script that accounts for tread orientation to sync up properly.

--- Assets/Scripts/Core/Player/PlayerMovement.cs
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -9,14 +9,24 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] InputReader inputReader;
     [SerializeField] Transform bodyTransform;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] ParticleSystem dustTrail;
 
     [Header("Settings")]
     [SerializeField] float movementSpeed = 4f;
     [SerializeField] float turningRate = 30f;
+    [SerializeField] float emissionRate = 10f;
 
+    private ParticleSystem.EmissionModule emissionModule;
     private Vector2 previousMovementInput;
+    private Vector3 previousPos;
 
-    
+    private const float ParticleStopThreshold = 0.005f;
+
+    private void Awake()
+    {
+        emissionModule = dustTrail.emission;
+    }
+
     public override void OnNetworkSpawn()
     {
         if(!IsOwner) { return; };
@@ -41,6 +51,17 @@ public class PlayerMovement : NetworkBehaviour
 
     private void FixedUpdate()
     {
+        if ((transform.position - previousPos).sqrMagnitude > ParticleStopThreshold)
+        {
+            emissionModule.rateOverTime = emissionRate;
+        }
+        else
+        {
+            emissionModule.rateOverTime = 0;
+        }
+
+        previousPos = transform.position;
+
         if (!IsOwner) { return; }
 
         rb.velocity = (Vector2)bodyTransform.up * previousMovementInput.y * movementSpeed;
